e74d2c5af483244ef33002457ccf2063cd149c6b
ILIA TIMOFEEV <ilya48007@gmail.com>
2023-12-13 20:26:28 +0300

    init

--- client/src/components/auth/auth.tsx
+++ client/src/components/auth/auth.tsx
@@ -1,25 +1,42 @@
 import React from "react";
 import { Login } from "../login";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { IState } from "../../store/modules";
 import { useLogin } from "./hooks/useLogin";
 import { useLogout } from "./hooks/useLogout";
-
+import { setStateAuthErrors } from "../../store/modules/auth";
 
 export const Auth = () => {
+  const loginFn = useLogin();
+  const logoutFn = useLogout();
+  const dispatch = useDispatch();
+  const login = useSelector((state: IState) => state.auth.login);
+  const password = useSelector((state: IState) => state.auth.password);
 
-    const loginFn = useLogin()
-    const logoutFn = useLogout()
-    const handleLogin = () => {
-        loginFn()
-        return true
+  const handleLogin = () => {
+    const error = {
+      login: login === "",
+      password: password === "",
+    };
+
+    if (Object.values(error).every((el) => !el)) {
+      loginFn();
+      return true;
     }
 
-    const isAuth = useSelector((state: IState) => !!state.auth.token)
-    const userName = useSelector((state: IState) => state.auth.user?.name)
+    dispatch(setStateAuthErrors(error));
+    return false;
+  };
 
-    const login = <Login handleLogin={handleLogin} />
-    const logout = <button className="button" onClick={logoutFn}>Выход ({userName})</button>
+  const isAuth = useSelector((state: IState) => !!state.auth.token);
+  const userName = useSelector((state: IState) => state.auth.user?.name);
 
-    return isAuth ? logout : login
-}
+  const loginButton = <Login handleLogin={handleLogin} />;
+  const logoutButton = (
+    <button className="button" onClick={logoutFn}>
+      Выход ({userName})
+    </button>
+  );
+
+  return isAuth ? logoutButton : loginButton;
+};
